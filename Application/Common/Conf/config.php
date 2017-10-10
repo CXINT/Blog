8f75f74b09ee0ff3737f3f595f2476dc0eca1b40
CXINT <2411233465@qq.com>
2017-10-10 23:37:19 +0800

    First commit

---
+++ Application/Common/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+    //URL地址不区分大小写
+    'URL_CASE_INSENSITIVE' =>true,
+    'URL_MODEL'=>0,
+    'LOAD_EXT_CONFIG' => 'db',
+    'MD5_PRE' => 'sing_cms',
+    'HTML_FILE_SUFFIX' => '.html',
+//    '__CSS__' => '/news/Public/css',
+//    '__JS__' => '/news/Public/js',
+);
